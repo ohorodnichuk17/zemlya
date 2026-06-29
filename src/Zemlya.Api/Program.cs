50c63e08ba87946450f2f4e32a3f73b664d78597
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2026-06-29 14:37:03 +0300

    serviced to DI added

--- src/Zemlya.Api/Program.cs
+++ src/Zemlya.Api/Program.cs
@@ -1,22 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-
 using Zemlya.Api.Middleware;
-using Zemlya.Api.Abstractions;
-using Zemlya.Api.Infrastructure.Database;
-using Zemlya.Api.Infrastructure.Weather;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseNpgsql(connectionString));
-
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddPresentation();
-
-// Add services to the container.
-builder.Services.AddHttpClient<IWeatherService, WeatherService>();
+builder.Services.AddInfrastructureServices();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
