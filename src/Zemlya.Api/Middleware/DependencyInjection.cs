b28ac80d29bbdf732411bf31797080107a0f5fa8
zubnaz <nzubar4@gmail.com>
2026-06-29 13:50:39 +0300

    add dependency injection

---
+++ src/Zemlya.Api/Middleware/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿namespace Zemlya.Api.Middleware;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddCORS();
+        return services;
+    }
+    private static IServiceCollection AddCORS(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CustomCORS", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+        });
+        return services;
+    }
+}
--- src/Zemlya.Api/Program.cs
+++ src/Zemlya.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Zemlya.Api.Database;
+using Zemlya.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +9,15 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseNpgsql(connectionString));
 
+builder.Services.AddPresentation();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseCors("CustomCORS");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
