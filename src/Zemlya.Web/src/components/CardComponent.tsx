0ca3dddc2f924ea3b67dabeeadbc6b4e3b705d57
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2026-06-30 17:50:53 +0300

    frontend build fixed

--- src/Zemlya.Web/src/components/CardComponent.tsx
+++ src/Zemlya.Web/src/components/CardComponent.tsx
@@ -1,34 +1,33 @@
-import { Card, CardContent, Typography, CardActions, Button, Stack, Box } from "@mui/material"
+import { Card, CardContent, Typography, CardActions, Button, Box } from "@mui/material"
 import fieldBackground from "../assets/images/field-background.png"
 import { format } from "date-fns";
 import type { IFieldsResponse } from "../interfaces/fields/fields"
 
-
 export const CardComponent = (props: { cardInfo: IFieldsResponse }) => {
-    return (
-        <Card sx={{ width: 255,height:350 }}>
-            <img src={fieldBackground} alt="Field" style={{ width: '100%', height: '100px', borderRadius: '4px 4px 0px 0px' }} />
-            <CardContent>
+   return (
+      <Card sx={{ width: 255, height: 350 }}>
+         <img src={fieldBackground} alt="Field" style={{ width: '100%', height: '100px', borderRadius: '4px 4px 0px 0px' }} />
+         <CardContent>
 
-                <Typography variant="h5" gutterBottom sx={{ color: 'text.secondary', fontSize: 20 }}>
-                    {props.cardInfo.name}
-                </Typography>
-                <Box>
-                    <Typography variant="body2">🌾 Вид культури: {props.cardInfo.cropType}</Typography>
-                    <Typography variant="body2">🟤 Тип ґрунту: {props.cardInfo.soilType}</Typography>
-                    <Typography variant="body2">📐 Площа: {props.cardInfo.sizeHectares} ha</Typography>
-                    <Typography variant="body2">📍 {props.cardInfo.latitude}, {props.cardInfo.longitude}</Typography>
-                    <Typography variant="caption" color="text.secondary">
-                        Створено: {format(props.cardInfo.createdAt, 'dd/MM/yyyy')}
-                    </Typography>
-                    <br />
-                    <Typography variant="caption" color="text.secondary">
-                        Оновлено: {format(props.cardInfo.updatedAt, 'dd/MM/yyyy')}
-                    </Typography>
-                </Box>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>)
+            <Typography variant="h5" gutterBottom sx={{ color: 'text.secondary', fontSize: 20 }}>
+               {props.cardInfo.name}
+            </Typography>
+            <Box>
+               <Typography variant="body2">🌾 Вид культури: {props.cardInfo.cropType}</Typography>
+               <Typography variant="body2">🟤 Тип ґрунту: {props.cardInfo.soilType}</Typography>
+               <Typography variant="body2">📐 Площа: {props.cardInfo.sizeHectares} ha</Typography>
+               <Typography variant="body2">📍 {props.cardInfo.latitude}, {props.cardInfo.longitude}</Typography>
+               <Typography variant="caption" color="text.secondary">
+                  Створено: {format(props.cardInfo.createdAt, 'dd/MM/yyyy')}
+               </Typography>
+               <br />
+               <Typography variant="caption" color="text.secondary">
+                  Оновлено: {format(props.cardInfo.updatedAt, 'dd/MM/yyyy')}
+               </Typography>
+            </Box>
+         </CardContent>
+         <CardActions>
+            <Button size="small">Learn More</Button>
+         </CardActions>
+      </Card>)
 }
--- src/Zemlya.Web/src/pages/FieldsPage.tsx
+++ src/Zemlya.Web/src/pages/FieldsPage.tsx
@@ -5,73 +5,72 @@ import { getFieldsAsync } from "../redux/actions/fieldsActions";
 import type { IPagination } from "../interfaces/general";
 import { CardComponent } from "../components/CardComponent";
 
-
 export const FieldsPage = () => {
 
-    const [pagination, setPagination] = useState<IPagination>({ page: 0, sizeOfPage: 3 });
-    const dispatch = useAppDispatch();
-    const paginationFieldsResponse = useAppSelector(state => state.fieldsReducer.paginationFieldsResponse);
+   const [pagination, setPagination] = useState<IPagination>({ page: 0, sizeOfPage: 3 });
+   const dispatch = useAppDispatch();
+   const paginationFieldsResponse = useAppSelector(state => state.fieldsReducer.paginationFieldsResponse);
 
-    useEffect(() => {
-        dispatch(getFieldsAsync(pagination));
-    }, [pagination]);
-    useEffect(() => {
-        console.log(paginationFieldsResponse);
-    }, [paginationFieldsResponse]);
-    return (
-        <Container sx={{
+   useEffect(() => {
+      dispatch(getFieldsAsync(pagination));
+   }, [pagination]);
+   useEffect(() => {
+      console.log(paginationFieldsResponse);
+   }, [paginationFieldsResponse]);
+   return (
+      <Container sx={{
+         display: 'flex',
+         flexDirection: 'column',
+         height: '100%',
+         width: '100%',
+         backgroundColor: '#E8F5E9',
+         padding: "20px 50px",
+         boxSizing: "border-box"
+      }}>
+         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%', marginBottom: '20px' }}>
+            <Typography variant="h2" component="h1" gutterBottom>
+               Поля
+            </Typography>
+            <Button sx={{ marginLeft: 'auto', height: '40px' }} variant="text">Додати поле</Button>
+         </Box>
+
+         <Box
+            sx={{
+               display: 'grid',
+               gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+               overflowY: 'auto',
+               overflowX: 'hidden',
+               flex: 1,
+               gap: '20px',
+               maxHeight: '450px',
+               width: '100%',
+               minWidth: '300px',
+               marginBottom: '20px',
+               boxSizing: 'border-box',
+               alignItems: 'center',
+               justifyItems: 'center',
+            }}
+         >
+            {paginationFieldsResponse != null && paginationFieldsResponse.fields != null && paginationFieldsResponse.fields.map((field) => (
+               <><CardComponent key={field.id} cardInfo={field} />
+               </>
+            ))}
+
+         </Box>
+         <Box sx={{
             display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-            width: '100%',
-            backgroundColor: '#E8F5E9',
-            padding: "20px 50px",
-            boxSizing: "border-box"
-        }}>
-            <Box sx={{ display: 'flex', flexDirection: 'row',alignItems: 'center', width: '100%', marginBottom: '20px' }}>
-                <Typography variant="h2" component="h1" gutterBottom>
-                    Поля
-                </Typography>
-                <Button sx={{marginLeft: 'auto', height: '40px'}} variant="text">Додати поле</Button>
-            </Box>
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '20px'
+         }}>
+            {paginationFieldsResponse != null
+               && paginationFieldsResponse.totalCount > pagination.sizeOfPage
+               && <Pagination
+                  page={pagination.page + 1}
+                  count={Math.ceil(paginationFieldsResponse.totalCount / pagination.sizeOfPage)}
+                  onChange={(_, page) => setPagination({ ...pagination, page: page - 1 })} />}
+         </Box>
 
-            <Box
-                sx={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-                    overflowY: 'auto',
-                    overflowX: 'hidden',
-                    flex: 1,
-                    gap: '20px',
-                    maxHeight: '450px',
-                    width: '100%',
-                    minWidth: '300px',
-                    marginBottom: '20px',
-                    boxSizing: 'border-box',
-                    alignItems: 'center',
-                    justifyItems: 'center',
-                }}
-            >
-                {paginationFieldsResponse != null && paginationFieldsResponse.fields != null && paginationFieldsResponse.fields.map((field) => (
-                    <><CardComponent key={field.id} cardInfo={field} />
-                    </>
-                ))}
-
-            </Box>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: '20px'
-            }}>
-                {paginationFieldsResponse != null
-                    && paginationFieldsResponse.totalCount > pagination.sizeOfPage
-                    && <Pagination
-                        page={pagination.page + 1}
-                        count={Math.ceil(paginationFieldsResponse.totalCount / pagination.sizeOfPage)}
-                        onChange={(event, page) => setPagination({ ...pagination, page: page - 1 })} />}
-            </Box>
-
-        </Container>
-    )
+      </Container>
+   )
 }
